9e30eb058a2a68e6f494df30d52c539af783e34c
JaviR011 <126350198+JaviR011@users.noreply.github.com>
2024-07-16 14:33:42 -0600

    Update ejemplo.py

--- Formatos/JSON/ejemplo.py
+++ Formatos/JSON/ejemplo.py
@@ -2,18 +2,18 @@ import json
 
 #
 
-# print("--- python a json")
-# entero = 21
-# json_entero = json.dumps(entero)
-# print(entero)
-# print(json_entero)
-# print(type(json_entero))
+print("--- python a json")
+entero = 21
+json_entero = json.dumps(entero)
+print(entero)
+ print(json_entero)
+ print(type(json_entero))
 
-# print("-- Json  a python --")
+ print("-- Json  a python --")
 
-# cadena_json = json.loads(json_entero)
-# print(cadena_json)
-# print(type(cadena_json))
+ cadena_json = json.loads(json_entero)
+ print(cadena_json)
+ print(type(cadena_json))
 
 diccionario = {"numero 1":1 , "Numero 2": 5 , "Numero 3":3}
 
